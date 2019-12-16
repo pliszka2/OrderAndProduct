41721d41ddfa1f4d0053653f37dacb1f0ae71b71
Krzysztof Tucholski <k.tucholski@gmail.com>
2019-12-16 09:56:01 +0100

    Export class instead of implementation from /src/Ecommerce

--- http/routes/cart-create/handler.ts
+++ http/routes/cart-create/handler.ts
@@ -1,12 +1,16 @@
 import fastify from 'fastify'
 import * as http from 'http'
 import { Ecommerce } from '../../../src/main/Ecommerce'
+import { Cart } from 'src/main/domain/Cart/Cart'
+import { InMemoryRepository } from 'src/main/common/InMemoryRepository'
 
 export const handler = async (
   _req: fastify.FastifyRequest<http.IncomingMessage>,
   reply: fastify.FastifyReply<http.ServerResponse>,
 ) => {
-  const result = await Ecommerce.CreateCartHandler.execute()
+  const result = await new Ecommerce.CreateCartHandler(
+    new InMemoryRepository<Cart>(),
+  ).execute()
 
   reply.send(result)
 }
--- src/main/Ecommerce.ts
+++ src/main/Ecommerce.ts
@@ -1,18 +1,6 @@
-import { EventPublisher } from './common/EventPublisher'
-import { ProductDomainEventHandler } from './domain/Product/ProductDomainEventHandler'
-import { InMemoryRepository } from './common/InMemoryRepository'
-import { Cart } from './domain/Cart/Cart'
-import { Product } from './domain/Product/Product'
 import { EcommerceApplicationService } from './application/EcommerceApplicationService'
-import { sampleListOfProducts } from './sample-data'
 
 export const Ecommerce = {
-  AddCartItemHandler: new EcommerceApplicationService.AddItemToCartCommandHandler(
-    new EventPublisher([new ProductDomainEventHandler()]),
-    new InMemoryRepository<Cart>(),
-    new InMemoryRepository<Product>(sampleListOfProducts),
-  ),
-  CreateCartHandler: new EcommerceApplicationService.CreateCardCommandHandler(
-    new InMemoryRepository<Cart>(),
-  ),
+  AddCartItemHandler: EcommerceApplicationService.AddItemToCartCommandHandler,
+  CreateCartHandler: EcommerceApplicationService.CreateCardCommandHandler,
 }
